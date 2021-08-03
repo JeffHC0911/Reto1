b5f9e808c18245eaa92c2feb5943c1865b844230
JeffHC0911 <jefferhc911@gmail.com>
2021-08-02 22:31:00 -0500

    Comm Inicial

---
+++ src/main/java/utp/com/App.java
@@ -0,0 +1,21 @@
+package utp.com;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        RenProyec r = new RenProyec(2, 50000000.0, 6.2);
+
+
+
+        System.out.println(r.calcularInteresSimple());
+        System.out.println(r.calcularInteresCompuesto());
+        System.out.println(r.analisisInversion());
+    }
+}
